14457eebfe71c3eb6f7b7625f6487b9d4bbb9267
nameless kid <107755554+sojupatil@users.noreply.github.com>
2023-04-10 00:11:52 +0530

    3

---
+++ js/sg.js
@@ -0,0 +1,42 @@
+var audio = document.getElementById("source");
+    var playPauseButton = document.getElementById("playpause");
+    var seekslider = document.getElementById("seekslider");
+    var curtimetext = document.getElementById("curtimetext");
+    var durtimetext = document.getElementById("durtimetext");
+    var isPlaying = false;
+
+    playPauseButton.addEventListener("click", function() {
+        if (isPlaying) {
+            audio.pause();
+            playPauseButton.innerHTML = "PLAY";
+        } else {
+            audio.play();
+            playPauseButton.innerHTML = "PAUSE";
+        }
+        isPlaying = !isPlaying;
+    });
+
+    audio.addEventListener("timeupdate", function() {
+        var curtime = parseInt(audio.currentTime, 10);
+        var durtime = parseInt(audio.duration, 10);
+        curtimetext.innerHTML = formatTime(curtime);
+        durtimetext.innerHTML = formatTime(durtime);
+        if (!isNaN(durtime)) {
+            seekslider.max = durtime;
+            seekslider.disabled = false;
+        }
+        seekslider.value = audio.currentTime;
+    });
+
+    seekslider.addEventListener("input", function() {
+        audio.currentTime = seekslider.value;
+    });
+
+    function formatTime(secs) {
+        var minutes = Math.floor(secs / 60);
+        var seconds = secs - (minutes * 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        return minutes + ":" + seconds;
+    }
