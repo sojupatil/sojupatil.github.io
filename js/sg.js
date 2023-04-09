107d64af79ae5b7151c24d2a621116c565a185de
LyubomirT <terlyubomir@gmail.com>
2023-04-09 22:16:59 +0300

    Contributed

--- js/sg.js
+++ js/sg.js
@@ -1,42 +1,46 @@
-var audio = document.getElementById("source");
-    var playPauseButton = document.getElementById("playpause");
-    var seekslider = document.getElementById("seekslider");
-    var curtimetext = document.getElementById("curtimetext");
-    var durtimetext = document.getElementById("durtimetext");
-    var isPlaying = false;
+const audio = document.getElementById("source");
+const playPauseButton = document.getElementById("playpause");
+const seekslider = document.getElementById("seekslider");
+const curtimetext = document.getElementById("curtimetext");
+const durtimetext = document.getElementById("durtimetext");
+let isPlaying = false;
 
-    playPauseButton.addEventListener("click", function() {
-        if (isPlaying) {
-            audio.pause();
-            playPauseButton.innerHTML = "PLAY";
-        } else {
-            audio.play();
-            playPauseButton.innerHTML = "PAUSE";
-        }
-        isPlaying = !isPlaying;
-    });
+playPauseButton.addEventListener("click", function() {
+  if (isPlaying) {
+    audio.pause();
+    playPauseButton.innerHTML = "PLAY";
+  } else {
+    audio.play();
+    playPauseButton.innerHTML = "PAUSE";
+  }
+  isPlaying = !isPlaying;
+});
 
-    audio.addEventListener("timeupdate", function() {
-        var curtime = parseInt(audio.currentTime, 10);
-        var durtime = parseInt(audio.duration, 10);
-        curtimetext.innerHTML = formatTime(curtime);
-        durtimetext.innerHTML = formatTime(durtime);
-        if (!isNaN(durtime)) {
-            seekslider.max = durtime;
-            seekslider.disabled = false;
-        }
-        seekslider.value = audio.currentTime;
-    });
+audio.addEventListener("timeupdate", function() {
+  const curtime = parseInt(audio.currentTime, 10);
+  const durtime = parseInt(audio.duration, 10);
+  curtimetext.innerHTML = formatTime(curtime);
+  durtimetext.innerHTML = formatTime(durtime);
+  if (!isNaN(durtime) && durtime !== 0) {
+    seekslider.max = durtime;
+    seekslider.disabled = false;
+  }
+  seekslider.value = audio.currentTime;
+});
 
-    seekslider.addEventListener("input", function() {
-        audio.currentTime = seekslider.value;
-    });
+seekslider.addEventListener("input", function() {
+  audio.currentTime = seekslider.value;
+});
 
-    function formatTime(secs) {
-        var minutes = Math.floor(secs / 60);
-        var seconds = secs - (minutes * 60);
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        return minutes + ":" + seconds;
-    }
+function formatTime(secs) {
+  const minutes = Math.floor(secs / 60);
+  let seconds = secs - (minutes * 60);
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  return `${minutes}:${seconds}`;
+}
+
+audio.addEventListener("error", function() {
+  alert("Error: Unable to load audio file");
+});
